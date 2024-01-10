91f82e96e1b34cbb442bf7888e4dad7b6ad8b257
abhay8696 <kambleabhaygautam@gmail.com>
2024-01-11 00:42:51 +0530

    created responsive navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+//styles
+import "./Navbar.css";
+import menuIcon from "../assets/menuIcon.svg"
+
+
+const NavList = ({ulClasses, liClasses})=>{
+
+    return (
+        <ul className = {ulClasses}>
+            <li id="delay1" className={liClasses}>Projects</li>
+            <li id="delay2" className={liClasses}>About</li>
+            <li id="delay3" className={liClasses}>Contact</li>
+        </ul>
+    )
+}
+
+const Navbar = () => {
+    const [navLinkPosition, setNavlinkPosition] = useState("linkOut");
+
+    const handleClick = ()=>{
+        const pos = navLinkPosition === "linkOut" ? "linkIn" : "linkOut";
+        setNavlinkPosition(pos)
+    }
+    return (
+        <nav>
+            <h1 className='brand'>{'</>Abhay'}</h1>
+            <NavList ulClasses = "navList navList-bg" liClasses = "navLinks navLinks-bg"/>
+            <NavList ulClasses = {`navList navList-sm`} liClasses = {`navLinks navLinks-sm ${navLinkPosition}`}/>
+            <img className='menuIcon' src={menuIcon} onClick={handleClick}/>
+        </nav>
+    );
+};
+
+export default Navbar;
