f969c6572607f944ec2b48f2415ab21243279322
abhay8696 <kambleabhaygautam@gmail.com>
2024-04-28 16:06:53 +0530

    created screenshots images and gifs of all  projects used in projectsData array

--- functions/projectsData.js
+++ functions/projectsData.js
@@ -1,3 +1,28 @@
+import medifyImg from "../src/assets/projects/medify.jpg";
+import medifyGif from "../src/assets/projects/medifyGif.gif";
+import lawFirmImg from "../src/assets/projects/lawFirm.jpg";
+import lawFirmGif from "../src/assets/projects/lawFirmGif.gif";
+import sunnysideImg from "../src/assets/projects/sunnyside.jpg";
+import sunnysideGif from "../src/assets/projects/sunnysideGif.gif";
+import qkartImg from "../src/assets/projects/qkart.jpg";
+import qkartGif from "../src/assets/projects/qkartGif.gif";
+import productsPageImg from "../src/assets/projects/productsPage.jpg";
+import productsPageGif from "../src/assets/projects/productsPageGif.gif";
+import multiStepFormImg from "../src/assets/projects/multiStepForm.jpg";
+import multiStepFormGif from "../src/assets/projects/multiStepFormGif.gif";
+import quoReactDorImg from "../src/assets/projects/quoReactDor.jpg";
+import quoReactDorGif from "../src/assets/projects/quoReactDorGif.gif";
+import mazeRunnerImg from "../src/assets/projects/mazeRunner.jpg";
+import mazeRunnerGif from "../src/assets/projects/mazeRunnerGif.gif";
+import qtifyImg from "../src/assets/projects/qtify.jpg";
+import qtifyGif from "../src/assets/projects/qtifyGif.gif";
+import countriesImg from "../src/assets/projects/countries.jpg";
+import countriesGif from "../src/assets/projects/countriesGif.gif";
+import ipAddressTrackerImg from "../src/assets/projects/ipAddressTracker.jpg";
+import ipAddressTrackerGif from "../src/assets/projects/ipAddressTrackerGif.gif";
+import logisticsCompanyImg from "../src/assets/projects/logisticsCompany.jpg";
+import logisticsCompanyGif from "../src/assets/projects/logisticsCompanyGif.gif";
+
 
 const landingPages = [
     {
@@ -5,30 +30,36 @@ const landingPages = [
         subtext: "React-based web application/landing page that allows users to find medical centers in a specific state and city across the USA and book appointments.",
         link: "https://medify-azure.vercel.app/",
         cardName: "medify",
-        github: "https://github.com/abhay8696/medify"
+        github: "https://github.com/abhay8696/medify",
+        img: medifyImg,
+        gif: medifyGif
     },
     {
         head: "law firm",
         subtext: "A dynamic Law Firm Landing Page developed from scratch, under the guidance of judges and mentors at the court of Crio.Do",
         link: "https://law-firm-landing-page-ruddy.vercel.app/",
         cardName: "lawFirm",
-        github: "https://github.com/abhay8696/law-firm-landing-page"
+        github: "https://github.com/abhay8696/law-firm-landing-page",
+        img: lawFirmImg,
+        gif: lawFirmGif
     },
     {
         head: "sunnyside",
         subtext: "Sunnyside agency landing page, Frontend Mentor challenge.",
         link: "https://abhay8696.github.io/sunnyside/",
         cardName: "sunnyside",
-        github: "https://github.com/abhay8696/sunnyside"
-        //
+        github: "https://github.com/abhay8696/sunnyside",
+        img: sunnysideImg,
+        gif: sunnysideGif
     },
     {
         head: "logistics company",
         subtext: "An SEO friendly web page which features a clean and intuitive layout, containing essential sections that give general information about company, and an easily accessible contact information.",
         link: "https://abhay8696.github.io/logistics-company/",
         cardName: "logisticsCompany",
-        github: "https://github.com/abhay8696/logistics-company"
-        //
+        github: "https://github.com/abhay8696/logistics-company",
+        img: logisticsCompanyImg,
+        gif: logisticsCompanyGif
     },
 
 ]
@@ -38,32 +69,36 @@ const ecomArr = [
         head: "qkart",
         subtext: "QKart is an E-commerce application offering a variety of products for customers to choose from.",
         link: "https://abhay8696-qkart-frontend.netlify.app/",
-        cardName: "sunnyside",
-        github: "https://github.com/abhay8696/QKART_FRONTEND"
+        cardName: "qkart",
+        github: "https://github.com/abhay8696/QKART_FRONTEND",
+        img: qkartImg,
+        gif: qkartGif
     },
     {
         head: "Frontend Mentor - E-commerce product page",
         subtext: "Frontend Mentor challange: The challenge was to build out an e-commerce product page and get it looking as close to the provided design as possible.",
         link: "https://abhay8696.github.io/e_com_page/",
         cardName: "productsPage",
-        github: "https://github.com/abhay8696/e_com_page"
-        //
+        github: "https://github.com/abhay8696/e_com_page",
+        img: productsPageImg,
+        gif: productsPageGif
     },
-    {
-        head: "qtrip",
-        subtext: "QTrip is a travel website aimed at travellers looking for a multitude of adventures in different cities. Created web pages using HTML and CSS and made them dynamic using JavaScript",
-        link: "https://qtrip-dynamic-frontend-abhay8696.netlify.app/",
-        cardName: "qtrip",
-        github: "https://github.com/abhay8696/kambleabhaygautam-ME_QTRIPDYNAMIC"
-        //
-    },
+    // {
+    //     head: "qtrip",
+    //     subtext: "QTrip is a travel website aimed at travellers looking for a multitude of adventures in different cities. Created web pages using HTML and CSS and made them dynamic using JavaScript",
+    //     link: "https://qtrip-dynamic-frontend-abhay8696.netlify.app/",
+    //     cardName: "qtrip",
+    //     github: "https://github.com/abhay8696/kambleabhaygautam-ME_QTRIPDYNAMIC"
+    //     //
+    // },
     {
         head: "Multi-step form",
         subtext: "Frontend Mentor challenge: A simple multi-step form, where information is stored using react-useState hook. App remembers and transfers data filled in form to next steps.",
         link: "https://abhay8696.github.io/multi-step-form/",
         cardName: "multiStepForm",
-        github: "https://github.com/abhay8696/multi-step-form"
-        //
+        github: "https://github.com/abhay8696/multi-step-form",
+        img: multiStepFormImg,
+        gif: multiStepFormGif
     },
 ]
 
@@ -73,8 +108,11 @@ const gamesArr = [
         subtext: "Successfully conceptualized and developed a visually appealing 2D board game using React. Leveraged Firestore database to enable real-time updates for multiplayer functionality, allowing seamless gameplay across remote locations.",
         link: "https://quo-react-dor.vercel.app/",
         cardName: "quoReactDor",
-        github: "https://github.com/abhay8696/quo-react-dor"
-        //
+        github: "https://github.com/abhay8696/quo-react-dor",
+        img: quoReactDorImg,
+        gif: quoReactDorImg,
+        // gif: quoReactDorGif,
+        //gif
     },
 ]
 
@@ -82,10 +120,11 @@ const psdsArr = [
     {
         head: "Recursive Maze Runner",
         subtext: "The simple UI app that generates and solves mazes dynamically with back-tracking",
-        link: "https://quo-react-dor.vercel.app/",
+        link: "https://abhay8696.github.io/maze_runner/",
         cardName: "mazeRunner",
-        github: "https://abhay8696.github.io/maze_runner/"
-        //
+        github: "https://github.com/abhay8696/maze_runner",
+        img: mazeRunnerImg,
+        gif: mazeRunnerGif
     },
 ]
 
@@ -95,23 +134,26 @@ const apiArr = [
         subtext: "QTify is a song-browsing platform built from scratch using ReactJS paired with Material UI to deliver a seamless and aesthetic user interface.",
         link: "https://qtify-alpha.vercel.app/",
         cardName: "qtify",
-        github: "https://github.com/abhay8696/qtify/"
-        //
+        github: "https://github.com/abhay8696/qtify/",
+        img: qtifyImg,
+        gif: qtifyGif
     },
     {
         head: "REST Countries API",
         subtext: "REST Countries API with color theme switcher. The App is integrated with the REST Countries API to pull country data.",
         link: "https://abhay8696.github.io/countries-api/",
         cardName: "countries",
-        github: "https://github.com/abhay8696/countries-api"
-        //
+        github: "https://github.com/abhay8696/countries-api",
+        img: countriesImg,
+        gif: countriesGif
     },
     {
         head: "IP address tracker",
         subtext: "This apps is used to get the IP Address locations, it used the IP Geolocation API by IPify. To generate the map, we recommend using LeafletJS.",
         link: "https://abhay8696.github.io/ipAddressTracker/",
         cardName: "ipAddressTracker",
-        github: "https://github.com/abhay8696/ipAddressTracker"
-        //
+        github: "https://github.com/abhay8696/ipAddressTracker",
+        img: ipAddressTrackerImg,
+        gif: ipAddressTrackerGif
     },
 ]
