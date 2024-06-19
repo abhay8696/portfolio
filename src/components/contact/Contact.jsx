72ecc0ceda318d0077a81309dd908d78ca272ebd
abhay8696 <kambleabhaygautam@gmail.com>
2024-06-19 20:22:04 +0530

    changed title in index.html

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+//styles
+import "./Contact.css";
+
+const Contact = () => {
+    return (
+        <div id='contact' className='Contact commonWrapper'>
+            <h1>Contact</h1>
+        </div>
+    );
+};
+
+export default Contact;
