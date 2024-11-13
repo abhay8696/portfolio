7749bc1e26fe7bf001ff7e67db0130805ddd023b
abhay8696 <kambleabhaygautam@gmail.com>
2024-11-13 21:38:25 +0530

    added Entertainment Web App project object (screenshot and gif) to projectsData

--- functions/projectsData.js
+++ functions/projectsData.js
@@ -22,26 +22,29 @@ import ipAddressTrackerImg from "../src/assets/projects/ipAddressTracker.jpg";
 import ipAddressTrackerGif from "../src/assets/projects/ipAddressTrackerGif.gif";
 import logisticsCompanyImg from "../src/assets/projects/logisticsCompany.jpg";
 import logisticsCompanyGif from "../src/assets/projects/logisticsCompanyGif.gif";
-
+import entertainmentWebAppImg from "../src/assets/projects/entertainmentWebApp.png";
+import entertainmentWebAppGif from "../src/assets/projects/entertainmentWebApp.gif";
 
 export const landingPagesArr = [
     {
         head: "medify",
-        subtext: "React-based web application/landing page that allows users to find medical centers in a specific state and city across the USA and book appointments.",
+        subtext:
+            "React-based web application/landing page that allows users to find medical centers in a specific state and city across the USA and book appointments.",
         link: "https://medify-azure.vercel.app/",
         cardName: "medify",
         github: "https://github.com/abhay8696/medify",
         img: medifyImg,
-        gif: medifyGif
+        gif: medifyGif,
     },
     {
         head: "law firm",
-        subtext: "A dynamic Law Firm Landing Page developed from scratch, under the guidance of judges and mentors at the court of Crio.Do",
+        subtext:
+            "A dynamic Law Firm Landing Page developed from scratch, under the guidance of judges and mentors at the court of Crio.Do",
         link: "https://law-firm-landing-page-ruddy.vercel.app/",
         cardName: "lawFirm",
         github: "https://github.com/abhay8696/law-firm-landing-page",
         img: lawFirmImg,
-        gif: lawFirmGif
+        gif: lawFirmGif,
     },
     {
         head: "sunnyside",
@@ -50,38 +53,40 @@ export const landingPagesArr = [
         cardName: "sunnyside",
         github: "https://github.com/abhay8696/sunnyside",
         img: sunnysideImg,
-        gif: sunnysideGif
+        gif: sunnysideGif,
     },
     {
         head: "logistics company",
-        subtext: "An SEO friendly web page which features a clean and intuitive layout, containing essential sections that give general information about company, and an easily accessible contact information.",
+        subtext:
+            "An SEO friendly web page which features a clean and intuitive layout, containing essential sections that give general information about company, and an easily accessible contact information.",
         link: "https://abhay8696.github.io/logistics-company/",
         cardName: "logisticsCompany",
         github: "https://github.com/abhay8696/logistics-company",
         img: logisticsCompanyImg,
-        gif: logisticsCompanyGif
+        gif: logisticsCompanyGif,
     },
-
-]
+];
 
 export const ecomArr = [
     {
         head: "qkart",
-        subtext: "QKart is an E-commerce application offering a variety of products for customers to choose from.",
+        subtext:
+            "QKart is an E-commerce application offering a variety of products for customers to choose from.",
         link: "https://abhay8696-qkart-frontend.netlify.app/",
         cardName: "qkart",
         github: "https://github.com/abhay8696/QKART_FRONTEND",
         img: qkartImg,
-        gif: qkartGif
+        gif: qkartGif,
     },
     {
         head: "Frontend Mentor - E-commerce product page",
-        subtext: "Frontend Mentor challange: The challenge was to build out an e-commerce product page and get it looking as close to the provided design as possible.",
+        subtext:
+            "Frontend Mentor challange: The challenge was to build out an e-commerce product page and get it looking as close to the provided design as possible.",
         link: "https://abhay8696.github.io/e_com_page/",
         cardName: "productsPage",
         github: "https://github.com/abhay8696/e_com_page",
         img: productsPageImg,
-        gif: productsPageGif
+        gif: productsPageGif,
     },
     // {
     //     head: "qtrip",
@@ -93,19 +98,21 @@ export const ecomArr = [
     // },
     {
         head: "Multi-step form",
-        subtext: "Frontend Mentor challenge: A simple multi-step form, where information is stored using react-useState hook. App remembers and transfers data filled in form to next steps.",
+        subtext:
+            "Frontend Mentor challenge: A simple multi-step form, where information is stored using react-useState hook. App remembers and transfers data filled in form to next steps.",
         link: "https://abhay8696.github.io/multi-step-form/",
         cardName: "multiStepForm",
         github: "https://github.com/abhay8696/multi-step-form",
         img: multiStepFormImg,
-        gif: multiStepFormGif
+        gif: multiStepFormGif,
     },
-]
+];
 
 export const gamesArr = [
     {
         head: "QUO-REACT-DOR - Online Board Game",
-        subtext: "Successfully conceptualized and developed a visually appealing 2D board game using React. Leveraged Firestore database to enable real-time updates for multiplayer functionality, allowing seamless gameplay across remote locations.",
+        subtext:
+            "Successfully conceptualized and developed a visually appealing 2D board game using React. Leveraged Firestore database to enable real-time updates for multiplayer functionality, allowing seamless gameplay across remote locations.",
         link: "https://quo-react-dor.vercel.app/",
         cardName: "quoReactDor",
         github: "https://github.com/abhay8696/quo-react-dor",
@@ -114,46 +121,60 @@ export const gamesArr = [
         // gif: quoReactDorGif,
         //gif
     },
-]
+];
 
 export const psdsArr = [
     {
         head: "Recursive Maze Runner",
-        subtext: "The simple UI app that generates and solves mazes dynamically with back-tracking",
+        subtext:
+            "The simple UI app that generates and solves mazes dynamically with back-tracking",
         link: "https://abhay8696.github.io/maze_runner/",
         cardName: "mazeRunner",
         github: "https://github.com/abhay8696/maze_runner",
         img: mazeRunnerImg,
-        gif: mazeRunnerGif
+        gif: mazeRunnerGif,
     },
-]
+];
 
 export const apiArr = [
     {
         head: "qtify",
-        subtext: "QTify is a song-browsing platform built from scratch using ReactJS paired with Material UI to deliver a seamless and aesthetic user interface.",
+        subtext:
+            "QTify is a song-browsing platform built from scratch using ReactJS paired with Material UI to deliver a seamless and aesthetic user interface.",
         link: "https://qtify-alpha.vercel.app/",
         cardName: "qtify",
         github: "https://github.com/abhay8696/qtify/",
         img: qtifyImg,
-        gif: qtifyGif
+        gif: qtifyGif,
     },
     {
         head: "REST Countries API",
-        subtext: "REST Countries API with color theme switcher. The App is integrated with the REST Countries API to pull country data.",
+        subtext:
+            "REST Countries API with color theme switcher. The App is integrated with the REST Countries API to pull country data.",
         link: "https://abhay8696.github.io/countries-api/",
         cardName: "countries",
         github: "https://github.com/abhay8696/countries-api",
         img: countriesImg,
-        gif: countriesGif
+        gif: countriesGif,
     },
     {
         head: "IP address tracker",
-        subtext: "This apps is used to get the IP Address locations, it used the IP Geolocation API by IPify. To generate the map, we recommend using LeafletJS.",
+        subtext:
+            "This apps is used to get the IP Address locations, it used the IP Geolocation API by IPify. To generate the map, we recommend using LeafletJS.",
         link: "https://abhay8696.github.io/ipAddressTracker/",
         cardName: "ipAddressTracker",
         github: "https://github.com/abhay8696/ipAddressTracker",
         img: ipAddressTrackerImg,
-        gif: ipAddressTrackerGif
+        gif: ipAddressTrackerGif,
     },
-]
+    {
+        head: "Entertainment web app",
+        subtext:
+            "An entertainment web app that connects to the TMDB API, featuring intuitive navigation across Home, Movies, TV Series, and Bookmarked Shows. Users can bookmark, search shows, and enjoy a responsive layout with smooth interactive elements. Local storage ensures quick access to saved content.",
+        link: "https://entertainment-web-app-flax.vercel.app/",
+        cardName: "entertainmentWebApp",
+        github: "https://github.com/abhay8696/entertainment-web-app",
+        img: entertainmentWebAppImg,
+        gif: entertainmentWebAppGif,
+    },
+];
