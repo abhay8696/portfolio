cad951e93cd000f08f184a1f60d4cc70883d19de
abhay8696 <kambleabhaygautam@gmail.com>
2024-11-16 21:07:12 +0530

    gave linkedIn, github links and updated display picture in Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 //styles
-import "./Hero.css"
-import myDP from "../../assets/me2.jpg";
+import "./Hero.css";
+import myDP from "../../assets/me3.jpeg";
 import instagramIcon from "../../assets/instagramIcon.svg";
 import linkedInIcon from "../../assets/linkedInIcon2.svg";
 import githubIcon from "../../assets/githubIcon.svg";
@@ -11,35 +11,49 @@ import gmailIcon from "../../assets/gmailIcon.svg";
  classNames: animeOnVisible, hoverAction are defined in app.css
 */
 
-const bio = "Once upon a time in a world filled with pixels and code, there was a coder who decided to embark on a quest to conquer the digital realm. Armed with a keyboard and a passion for pixels, he built many industry grade projects from scratch and got armed with many technological skills useful for tech projects."
+const bio =
+    "Once upon a time in a world filled with pixels and code, there was a coder who decided to embark on a quest to conquer the digital realm. Armed with a keyboard and a passion for pixels, he built many industry grade projects from scratch and got armed with many technological skills useful for tech projects.";
 const Hero = () => {
     return (
-        <header className='hero' id='hero'>
-            <div className='heroInto'>
-                <h1 id='hey'>hey!</h1>
-                <h1 id='iAm'>I'm</h1>
-                <img src={myDP} className='myDP'/>
-                <h1 id='abhay'>abhay.</h1>
+        <header className="hero" id="hero">
+            <div className="heroInto">
+                <h1 id="hey">hey!</h1>
+                <h1 id="iAm">I'm</h1>
+                <img src={myDP} className="myDP" />
+                <h1 id="abhay">abhay.</h1>
             </div>
-            <article className='animeOnVisible'>
+            <article className="animeOnVisible">
                 {bio}
-                <br/>
-                He is known as the 
-                <span id='designation' className='animeOnVisible'>software developer.</span>
+                <br />
+                He is known as the
+                <span id="designation" className="animeOnVisible">
+                    software developer.
+                </span>
             </article>
-            <div className='links'>
-                <div className='socialLinks animeOnVisible'>
-                    <a className='hoverAction' href='#' target='_blank'>
-                        <img src={linkedInIcon} alt='linkedIn icon' />
+            <div className="links">
+                <div className="socialLinks animeOnVisible">
+                    <a
+                        className="hoverAction"
+                        href="https://www.linkedin.com/in/abhay8696/"
+                        target="_blank"
+                    >
+                        <img src={linkedInIcon} alt="linkedIn icon" />
                     </a>
-                    <a className='hoverAction' href='#' target='_blank'>
-                        <img src={githubIcon} alt='github icon' />
+                    <a
+                        className="hoverAction"
+                        href="https://github.com/abhay8696/"
+                        target="_blank"
+                    >
+                        <img src={githubIcon} alt="github icon" />
                     </a>
-                    <a className='hoverAction' href='#' target='_blank'>
+                    {/* <a className='hoverAction' href='#' target='_blank'>
                         <img src={instagramIcon} alt='instagram icon' />
-                    </a>
+                    </a> */}
                 </div>
-                <div className='email animeOnVisible'>kambleabhaygautam@gmail.com</div>
+                <div className="email animeOnVisible">
+                    <img src={gmailIcon} alt="gmail icon" />
+                    <span>kambleabhaygautam@gmail.com</span>
+                </div>
             </div>
         </header>
     );
