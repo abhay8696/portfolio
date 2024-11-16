7246be75ad0fb007b43d24d6ef55903f4b372c3e
abhay8696 <kambleabhaygautam@gmail.com>
2024-11-16 20:24:53 +0530

    Created new description about me in About page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,32 +1,47 @@
-import React from 'react';
+import React from "react";
 //styles
 import "./About.css";
 //components
-import Skills from './Skills/Skills';
+import Skills from "./Skills/Skills";
 //assets
 import myDP from "../../assets/me2.jpg";
-import Services from './Services/Services';
+import Services from "./Services/Services";
+
+const para1 =
+    "With a Bachelor of Science and a rich blend of 9 years as a mechanical technician at Indian Railways, I bring a unique mix of hands-on technical experience and software development expertise. My career journey has led me to pursue my passion for technology and programming, where I now work as a dedicated software developer and freelancer, providing tailored tech solutions to clients globally.";
+const para2 =
+    "I specialize in backend development, frontend frameworks, and complete software project cycles. My technical background, coupled with my problem-solving mindset, enables me to approach software challenges with precision and innovation. Whether building robust web applications, developing secure and scalable backend systems, or optimizing performance, I bring a meticulous approach and dedication to every project.";
+const para3 =
+    "Currently based in Pune, India, I am committed to staying at the forefront of technology. As a freelancer, I work collaboratively with clients to understand their unique needs, delivering high-quality solutions that meet modern demands.";
 
 const About = () => {
     return (
-        <div className='About commonWrapper' id='about'>
-            <div className='aboutInfo'>
-                <aside className='about-leftSide'>
-                    <div className='aboutTop'>
-                        <div className='aboutTitle'>
-                            <h2 className='aboutName'>Abhay Kamble</h2>
-                            <h3 className='aboutRole'>Software Developer</h3>
+        <div className="About commonWrapper" id="about">
+            <div className="aboutInfo">
+                <aside className="about-leftSide">
+                    <div className="aboutTop">
+                        <div className="aboutTitle">
+                            <h2 className="aboutName">Abhay Kamble</h2>
+                            <h3 className="aboutRole">Software Developer</h3>
                         </div>
-                        <img src={myDP} alt='my image' className='aboutImage-left'/>
+                        <img
+                            src={myDP}
+                            alt="my image"
+                            className="aboutImage-left"
+                        />
                     </div>
-                    <div className='aboutTexts'>
-                        <p className='aboutPara'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est molestias ab voluptatibus possimus aspernatur impedit, cum a eos reiciendis voluptate, explicabo iste nemo. Ducimus a commodi beatae nihil ipsa modi culpa nisi odio maxime laborum, inventore alias, aspernatur facere doloremque quo corporis nostrum quisquam tenetur atque, asperiores officiis. Commodi, molestiae?</p>
-                        <p className='aboutPara'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est molestias ab voluptatibus possimus aspernatur impedit, cum a eos reiciendis voluptate, explicabo iste nemo. Ducimus a commodi beatae nihil ipsa modi culpa nisi odio maxime laborum, inventore alias, aspernatur facere doloremque quo corporis nostrum quisquam tenetur atque, asperiores officiis. Commodi, molestiae?</p>
-                        <p className='aboutPara'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est molestias ab voluptatibus possimus aspernatur impedit, cum a eos reiciendis voluptate, explicabo iste nemo. Ducimus a commodi beatae nihil ipsa modi culpa nisi odio maxime laborum, inventore alias, aspernatur facere doloremque quo corporis nostrum quisquam tenetur atque, asperiores officiis. Commodi, molestiae?</p>
+                    <div className="aboutTexts">
+                        <p className="aboutPara">{para1}</p>
+                        <p className="aboutPara">{para2}</p>
+                        <p className="aboutPara">{para3}</p>
                     </div>
                 </aside>
-                <aside className='about-rightSide'>
-                    <img src={myDP} alt='my image' className='aboutImage-right'/>
+                <aside className="about-rightSide">
+                    <img
+                        src={myDP}
+                        alt="my image"
+                        className="aboutImage-right"
+                    />
                 </aside>
             </div>
             <Services />
